ee07bb14697841e4828facfb323c2ec8ef0fc043
bekzhanne <137751649+bekzhanne@users.noreply.github.com>
2025-10-10 18:06:13 +0500

    Add files via upload

---
+++ go-practice3/cmd/verify/main.go
@@ -0,0 +1,34 @@
+﻿package main
+
+import (
+"database/sql"
+"fmt"
+"log"
+
+_ "modernc.org/sqlite"
+)
+
+func main() {
+db, err := sql.Open("sqlite", "./expense.db")
+if err != nil {
+log.Fatal("open:", err)
+}
+defer db.Close()
+
+if err := db.Ping(); err != nil {
+log.Fatal("ping:", err)
+}
+
+var cnt int
+err = db.QueryRow(`
+SELECT COUNT(*)
+FROM sqlite_master
+WHERE type='table'
+  AND name IN ('users','categories','expenses')
+`).Scan(&cnt)
+if err != nil {
+log.Fatal("query:", err)
+}
+
+fmt.Printf("OK. Tables found: %d (users, categories, expenses)\n", cnt)
+}
